53b7917b307cb5dbd018ca5019792a6775e921f9
AmirAli Golbon <aasg77@yahoo.com>
2026-05-15 13:35:35 +0330

    template
    template added

--- website/views.py
+++ website/views.py
@@ -3,10 +3,10 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index_view(request):
-    return HttpResponse('<h1>Home Page</h1>')
+    return render(request, 'website/index.html')
 
 def contacts_view(request):
-    return HttpResponse('<h1>Contacts</h1>')
+    return render(request, 'website/contact.html')
 
 def about_view(request):
-    return HttpResponse('<h1>About</h1>')
+    return render(request, 'website/about.html')
